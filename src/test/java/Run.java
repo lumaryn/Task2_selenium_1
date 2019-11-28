d6f25dde2047068adb71ad4166ba622f9d920674
Ivan Ivanov <Ivan@lanit.ru>
2019-11-28 09:48:44 +0300

    Initial commit

---
+++ src/test/java/Run.java
@@ -0,0 +1,8 @@
+public class Run {
+    public static void main(String[] args) throws Exception {
+
+        TaskOne run = new TaskOne();
+        run.testSber();
+        run.testInsurance();
+    }
+}
